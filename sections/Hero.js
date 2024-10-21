bd16e4e1e5d4dd5973f2dbf5b8648068960eeaeb
Mahmudulshawn <mahmudulalamshawn@gmail.com>
2024-10-21 19:57:20 +0600

    fix the star problem in the hero section

--- sections/Hero.js
+++ sections/Hero.js
@@ -31,43 +31,43 @@ const Hero = () => {
         <div className="size-[1020px] hero-ring"></div>
         <div className="size-[1220px] hero-ring"></div>
         <HeroOrbit size={800} rotation={-72}>
-          <StarIcon className="size-28 text-indigo-400/80" />
+          <StarIcon className="size-28 text-indigo-400" />
         </HeroOrbit>
         <HeroOrbit size={610} rotation={-120}>
-          <StarIcon className="size-20 text-indigo-300/70" />
+          <StarIcon className="size-20 text-indigo-300" />
         </HeroOrbit>
         <HeroOrbit size={750} rotation={70}>
-          <StarIcon className="size-16 text-indigo-300/70" />
+          <StarIcon className="size-16 text-indigo-300" />
         </HeroOrbit>
         <HeroOrbit size={450} rotation={160}>
-          <StarIcon className="size-10 text-indigo-300/50" />
+          <StarIcon className="size-10 text-indigo-300" />
         </HeroOrbit>
         <HeroOrbit size={550} rotation={20}>
-          <StarIcon className="size-12 text-indigo-300/80" />
+          <StarIcon className="size-12 text-indigo-300" />
         </HeroOrbit>
         <HeroOrbit size={590} rotation={98}>
-          <StarIcon className="size-8 text-indigo-300/80" />
+          <StarIcon className="size-8 text-indigo-300" />
         </HeroOrbit>
         <HeroOrbit size={430} rotation={-14}>
-          <SparkleIcon className="size-8 text-indigo-300/20" />
+          <SparkleIcon className="size-8 text-indigo-300" />
         </HeroOrbit>
         <HeroOrbit size={440} rotation={79}>
-          <SparkleIcon className="size-5 text-indigo-300/20" />
+          <SparkleIcon className="size-5 text-indigo-300" />
         </HeroOrbit>
         <HeroOrbit size={530} rotation={178}>
-          <SparkleIcon className="size-10 text-indigo-300/20" />
+          <SparkleIcon className="size-10 text-indigo-300" />
         </HeroOrbit>
         <HeroOrbit size={710} rotation={144}>
-          <SparkleIcon className="size-14 text-indigo-300/20" />
+          <SparkleIcon className="size-14 text-indigo-300" />
         </HeroOrbit>
         <HeroOrbit size={720} rotation={0}>
-          <div className="size-3 rounded-full bg-indigo-300/20" />
+          <div className="size-3 rounded-full bg-indigo-300" />
         </HeroOrbit>
         <HeroOrbit size={520} rotation={-41}>
-          <div className="size-2 rounded-full bg-indigo-300/20" />
+          <div className="size-2 rounded-full bg-indigo-300" />
         </HeroOrbit>
         <HeroOrbit size={650} rotation={-5}>
-          <div className="size-2 rounded-full bg-indigo-300/20" />
+          <div className="size-2 rounded-full bg-indigo-300" />
         </HeroOrbit>
       </div>
 
